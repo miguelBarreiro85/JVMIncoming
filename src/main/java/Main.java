fea31afe22cebe9f559c35a990b4c7e00d9d6461
mbarreiro <mbarreiro@observador.pt>
2025-05-31 10:59:02 +0100

    wip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,42 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
     try {
       ServerSocket serverSocket = new ServerSocket(4221);
-    
+
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-    
-      serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
+
+      do {
+        Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+
+        System.out.println("Connection established with client: " + clientSocket.getInetAddress());
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        String requestMessage = in.readLine();
+        System.out.println("The received message from the client: " + requestMessage);
+
+        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+
+        clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+        clientSocket.close();
+      } while (true); // Keep the server running indefinitely
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
